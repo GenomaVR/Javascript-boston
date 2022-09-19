1f8820fc644c9c119054865da1b017e3500088c6
Lucas Bernardez <genomarpg@gmail.com>
2022-09-19 17:52:50 -0300

    Jquerry and toast
    Se añadio con jquerry un toast en la pagina de inicio indicando que la pagina esta en desarrollo

---
+++ js/alert.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", function() {
+    toastr["success"]("Recuerda que la pagina esta en desarrollo, si notas algún error no dudes en informarlo en nuestras redes!", "Bienvenido a Boston Argentina!")
+
+    toastr.options = {
+      "closeButton": false,
+      "debug": false,
+      "newestOnTop": false,
+      "progressBar": false,
+      "positionClass": "toast-bottom-left",
+      "preventDuplicates": false,
+      "onclick": null,
+      "showDuration": "500",
+      "hideDuration": "1000",
+      "timeOut": "5000",
+      "extendedTimeOut": "1000",
+      "showEasing": "swing",
+      "hideEasing": "linear",
+      "showMethod": "fadeIn",
+      "hideMethod": "fadeOut"
+    };
+  })
